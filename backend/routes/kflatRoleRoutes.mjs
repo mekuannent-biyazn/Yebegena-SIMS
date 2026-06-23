48b564aede03291c8769aa0bc6b8b3605ee40052
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-23 23:14:37 +0300

    fiat: Create RESTfull kflatRole API

---
+++ backend/routes/kflatRoleRoutes.mjs
@@ -0,0 +1,26 @@
+import express from "express";
+
+import protect from "../middlewares/authMiddleware.mjs";
+import authorize from "../middlewares/roleMiddleware.mjs";
+
+import {
+  createKflatRole,
+  getAllKflatRoles,
+  getSingleKflatRole,
+  updateKflatRole,
+  deleteKflatRole,
+} from "../controllers/kflatRoleController.mjs";
+
+const router = express.Router();
+
+router.get("/", protect, getAllKflatRoles);
+
+router.get("/:id", protect, getSingleKflatRole);
+
+router.post("/", protect, authorize("ADMIN"), createKflatRole);
+
+router.put("/:id", protect, authorize("ADMIN"), updateKflatRole);
+
+router.delete("/:id", protect, authorize("ADMIN"), deleteKflatRole);
+
+export default router;
