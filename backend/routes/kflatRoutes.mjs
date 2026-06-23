9da8b50acde9ed915288581e9ce561b8f4376b27
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-23 23:12:37 +0300

    fiat: Create RESTfull kflat API

---
+++ backend/routes/kflatRoutes.mjs
@@ -0,0 +1,26 @@
+import express from "express";
+
+import protect from "../middlewares/authMiddleware.mjs";
+import authorize from "../middlewares/roleMiddleware.mjs";
+
+import {
+  createKflat,
+  getAllKflats,
+  getSingleKflat,
+  updateKflat,
+  deleteKflat,
+} from "../controllers/kflatController.mjs";
+
+const router = express.Router();
+
+router.get("/", protect, getAllKflats);
+
+router.get("/:id", protect, getSingleKflat);
+
+router.post("/", protect, authorize("ADMIN"), createKflat);
+
+router.put("/:id", protect, authorize("ADMIN"), updateKflat);
+
+router.delete("/:id", protect, authorize("ADMIN"), deleteKflat);
+
+export default router;
