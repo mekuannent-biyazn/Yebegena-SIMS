1b1e7c3da079f01e65d2db272df0f43b960a58ea
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-24 11:44:03 +0300

    feat: Create it for RESTfull API for students

---
+++ backend/routes/studentRoutes.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+
+import protect from "../middlewares/authMiddleware.mjs";
+import authorize from "../middlewares/roleMiddleware.mjs";
+
+import {
+  getMyProfile,
+  approveStudent,
+  assignStudentToClass,
+  getPendingStudents,
+  rejectStudent,
+  studentStats,
+} from "../controllers/studentController.mjs";
+
+const router = express.Router();
+
+router.get("/profile", protect, getMyProfile);
+
+router.get("/pending", protect, authorize("ADMIN"), getPendingStudents);
+
+router.put("/approve/:id", protect, authorize("ADMIN"), approveStudent);
+
+router.put("/assign-class", protect, authorize("ADMIN"), assignStudentToClass);
+
+router.put("/reject/:id", protect, authorize("ADMIN"), rejectStudent);
+
+router.get("/stats", protect, authorize("ADMIN"), studentStats);
+
+export default router;
