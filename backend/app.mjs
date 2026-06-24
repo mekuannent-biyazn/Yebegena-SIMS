3ce6cfab40a9af070e6c758cd8a626e50807758d
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-06-24 11:44:55 +0300

    feat: Add the student routers

--- backend/app.mjs
+++ backend/app.mjs
@@ -2,6 +2,12 @@ import express from "express";
 import cors from "cors";
 
 import corsOptions from "./config/cors.mjs";
+import authRoutes from "./routes/authRoutes.mjs";
+import kflatRoutes from "./routes/kflatRoutes.mjs";
+import kflatRoleRoutes from "./routes/kflatRoleRoutes.mjs";
+import teacherRoutes from "./routes/teacherRoutes.mjs";
+import classRoutes from "./routes/classRoutes.mjs";
+import studentRoutes from "./routes/studentRoutes.mjs";
 
 const app = express();
 
@@ -17,5 +23,12 @@ app.get("/", (req, res) => {
     message: "Yebegena SIMS API Running",
   });
 });
+
+app.use("/api", authRoutes);
+app.use("/api/kflats", kflatRoutes);
+app.use("/api/kflat-roles", kflatRoleRoutes);
+app.use("/api/teachers", teacherRoutes);
+app.use("/api/classes", classRoutes);
+app.use("/api/students", studentRoutes);
 
 export default app;
