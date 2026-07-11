79d660facc63a964c31cc8692aec7be37505fc92
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-07-11 23:19:04 +0300

    Fix the api

--- backend/config/cors.mjs
+++ backend/config/cors.mjs
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:5173" || "https://yebegena-sims.vercel.app",
+  origin: "http://localhost:5173" || "https://yebegena-sims.vercel.app/api",
   credentials: true,
 };
 
