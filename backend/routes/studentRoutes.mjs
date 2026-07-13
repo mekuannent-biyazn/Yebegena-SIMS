456353bfe868e57291e46b7e770292464eedeb70
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-07-13 16:01:40 +0300

    mdfy: Change The role for get students report

--- backend/routes/studentRoutes.mjs
+++ backend/routes/studentRoutes.mjs
@@ -19,8 +19,6 @@ const router = express.Router();
 
 router.get("/profile", protect, getMyProfile);
 
-router.get("/students", protect, authorize("TEACHER"), studentReport);
-
 router.get("/pending", protect, authorize("ADMIN"), getPendingStudents);
 
 router.put("/approve/:id", protect, authorize("ADMIN"), approveStudent);
@@ -30,6 +28,8 @@ router.put("/assign-class", protect, authorize("ADMIN"), assignStudentToClass);
 router.put("/reject/:id", protect, authorize("ADMIN"), rejectStudent);
 
 router.get("/stats", protect, authorize("ADMIN"), studentStats);
+
+router.get("/students", protect, authorize("ADMIN"), studentReport);
 
 router.get("/:id", protect, authorize("ADMIN"), getStudentById);
 
