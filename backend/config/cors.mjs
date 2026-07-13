ff5d7fbf245adbe987f0ca27a33b13fc6952da5f
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-07-13 12:40:49 +0300

    fix: CORS allowed origin issues

--- backend/config/cors.mjs
+++ backend/config/cors.mjs
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://yebegena-sims-q5jwa6er2-mekuannents-projects.vercel.app",
+    "https://yebegena-sims.vercel.app",
   ],
   credentials: true,
 };
